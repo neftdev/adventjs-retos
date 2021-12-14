0f3044d82ba7ea41017ba3e778feb6e6362bd47f
rberduo <berduoronald@gmail.com>
2021-12-13 23:17:22 -0600

    feat: Add solution challenge 12

--- challenge-11/solution.js
+++ challenge-11/solution.js
@@ -4,11 +4,11 @@ const fidelity_card_value = 250;
 
 export default function shouldBuyFidelity(times) {
 	const normal_entrance = ticket_value * times;
-	let mult = 0;
+	let discount  = 0;
 	while (times > 0) {
-		mult += Math.pow(percentage_value, times);
+		discount  += Math.pow(percentage_value, times);
 		times -= 1;
 	}
-	const fidelity = fidelity_card_value + mult * ticket_value;
+	const fidelity = fidelity_card_value + discount * ticket_value;
 	return fidelity < normal_entrance;
 }
