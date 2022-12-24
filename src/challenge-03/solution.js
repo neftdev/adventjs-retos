d97ab81072cd8a9fd481ae6a26d9c46f4a36f284
rberduo <berduoronald@gmail.com>
2022-12-23 21:52:01 -0600

    feat: solution for challenge 3

--- src/challenge-03/solution.js
+++ src/challenge-03/solution.js
@@ -1,7 +1,18 @@
 /**
- * @param {string[]} packOfGifts 
- * @param {string[]} reindeers 
+ * @param {string[]} packOfGifts names of gifts
+ * @param {string[]} reindeers names of reindeers
  */
 export function distributeGifts(packOfGifts, reindeers) {
+	const REINDEER_MULTIPLE = 2;
+	const giftsNames = packOfGifts.join('');
+	const reindeersNames = reindeers.join('');
 
+	const totalGiftWeight = giftsNames.length;
+	const totalReindeerWeightCapacity = reindeersNames.length * REINDEER_MULTIPLE;
+
+	const numberOfPacks = Math.floor(
+		totalReindeerWeightCapacity / totalGiftWeight
+	);
+
+	return numberOfPacks;
 }
