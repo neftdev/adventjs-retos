439ba800adc5d1d70861adb7079c14389e874ece
rberduo <berduoronald@gmail.com>
2022-12-23 21:14:00 -0600

    feat: solution for challenge 2

--- src/challenge-02/solution.js
+++ src/challenge-02/solution.js
@@ -1,8 +1,24 @@
 /**
+ * return the number of extra hours that would be done during that year
  * 
- * @param {number} year 
- * @param {string[]} holidays 
+ * @param {number} year
+ * @param {string[]} holidays
  */
 export function countHours(year, holidays) {
+	const EXTRA_HOURS_BY_DAY = 2;
 
+	const isWeekend = (holiday) => {
+		const SUNDAY = 0;
+		const SATURDAY = 6;
+		const date = new Date(`${year}/${holiday}`);
+		const valueDay = date.getDay();
+		return valueDay === SUNDAY || valueDay === SATURDAY;
+	};
+
+	const numberOfHolidays = holidays.length;
+	const numberOfHolidaysInWeekend = holidays.filter(isWeekend).length;
+	const numberOfHolidaysInWeek = numberOfHolidays - numberOfHolidaysInWeekend;
+	const extraHoursCurrentYear = numberOfHolidaysInWeek * EXTRA_HOURS_BY_DAY;
+
+	return extraHoursCurrentYear;
 }
